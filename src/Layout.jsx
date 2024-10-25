7a8f7d7353e318c9df95581666a2ffd03f9c0637
katharina-spiecker <kathispiecker@gmail.com>
2024-10-25 14:05:24 +0200

    format

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,19 +1,20 @@
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
-
-    return (
-        <>
-            <header className="bg-yellow-300">
-                <nav className="sm:container mx-auto py-3 px-4">
-                    <a className="me-7" href="/">Übersicht</a>
-                </nav>
-            </header>
-            <main className="bg-slate-100 pt-5 md:pt-10">
-                <div className="mx-auto max-w-2xl px-4">
-                    <Outlet />
-                </div>
-            </main>
-        </>
-    )
+  return (
+    <>
+      <header className="bg-yellow-300">
+        <nav className="sm:container mx-auto py-3 px-4">
+          <a className="me-7" href="/">
+            Alle Themen
+          </a>
+        </nav>
+      </header>
+      <main className="bg-slate-100 pt-5 md:pt-10">
+        <div className="mx-auto max-w-2xl px-4">
+          <Outlet />
+        </div>
+      </main>
+    </>
+  );
 }
